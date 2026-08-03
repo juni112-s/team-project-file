686f440d74ad2de29b713c9472d4fb5149857846
seong929 <rlgur0929@naver.com>
2026-08-03 14:48:35 +0900

    SecondPAge 수정본2

--- teamfile01_app/lib/view/second_page.dart
+++ teamfile01_app/lib/view/second_page.dart
@@ -13,6 +13,7 @@ class _SecondPageState extends State<SecondPage> {
   late TextEditingController num2controller;
 
   late TextEditingController subcontroller;
+  late String result;
 
   @override
   void initState() {
@@ -20,6 +21,7 @@ class _SecondPageState extends State<SecondPage> {
     num1controller = TextEditingController();
     num2controller = TextEditingController();
     subcontroller = TextEditingController();
+    result = '';
   }
 
   @override
@@ -31,32 +33,48 @@ class _SecondPageState extends State<SecondPage> {
       body: Center(
         child: Column(
           children: [
-            TextField(
-              controller: num1controller,
-              decoration: InputDecoration(
-                labelText: '첫번째 숫자를 입력하세요.',
-                border: OutlineInputBorder()
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: num1controller,
+                decoration: InputDecoration(
+                  labelText: '첫번째 숫자를 입력하세요.',
+                  border: OutlineInputBorder()
+                ),
+                keyboardType: TextInputType.number,
               ),
-              keyboardType: TextInputType.number,
             ),
-            TextField(
-              controller: num2controller,
-              decoration: InputDecoration(
-                labelText: '두번째 숫자를 입력하세요.',
-                border: OutlineInputBorder()
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: num2controller,
+                decoration: InputDecoration(
+                  labelText: '두번째 숫자를 입력하세요.',
+                  border: OutlineInputBorder()
+                ),
+                keyboardType: TextInputType.number,
               ),
-              keyboardType: TextInputType.number,
             ),
             SizedBox(height: 30,),
             ElevatedButton(
               onPressed: subAction,
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                backgroundColor: Colors.blue,
+                foregroundColor: Colors.white
+              ),
               child: Text('Ok'),
             ),
-            TextField(
-              controller: subcontroller,
-              decoration: InputDecoration(
-                labelText: '뺄셈 결과',
-                border: OutlineInputBorder()
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: subcontroller,
+                decoration: InputDecoration(
+                  labelText: '뺄셈 결과',
+                  border: OutlineInputBorder()
+                ),
               ),
             )
           ],
@@ -66,12 +84,15 @@ class _SecondPageState extends State<SecondPage> {
   }
 
   void subAction(){
-    int num1 = int.parse(num1controller.text);
-    int num2 = int.parse(num2controller.text);
+    int num1 = int.parse(num1controller.text.trim());
+    int num2 = int.parse(num2controller.text.trim());
 
     int subresult = num1 - num2;
 
-    subcontroller.text = subresult.toString();
+    subcontroller.text = result;
+    result = '$num1 - $num2 = $subresult';
+    setState(() {});
+
 
   }
 }
