4a15aff05b9bd5886aaba92b7a6f3f7feded6546
lgh877 <lgh877@naver.com>
2026-08-03 13:48:34 +0900

    나누기 계산기 완료

--- teamfile01_app/lib/view/four_page.dart
+++ teamfile01_app/lib/view/four_page.dart
@@ -8,15 +8,137 @@ class FourPage extends StatefulWidget {
 }
 
 class _FourPageState extends State<FourPage> {
+  ///입력을 받을 텍스트 에디팅 컨트롤러들
+  late TextEditingController input1Controller, input2Controller;
+  ///출력값을 저장하는 문자열
+  late String resultString;
+
+  @override
+  void initState() {
+    super.initState();
+    input1Controller = TextEditingController();
+    input2Controller = TextEditingController();
+    resultString = '계산 대기중';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Four Page'),
+        title: Text('나눗셈'),
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.red,
       ),
       body: Center(
-        child: Text('네번째 페이지'),
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            SizedBox(
+              width: 150,
+              height: 50,
+              child: TextField(
+                controller: input1Controller,
+                decoration: InputDecoration(
+                  labelText: '분자 입력',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            SizedBox(height: 25),
+            SizedBox(
+              width: 150,
+              height: 50,
+              child: TextField(
+                controller: input2Controller,
+                decoration: InputDecoration(
+                  labelText: '분모 입력',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            SizedBox(height: 25),
+            ElevatedButton(
+              onPressed: onPressedCalculateButton, 
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5)
+                ),
+                textStyle: TextStyle(
+                  fontWeight: .w900,
+                  fontSize: 20
+                )
+              ),
+              child: Text('계산 및 출력')
+            ),
+            SizedBox(height: 50),
+            Text(
+              resultString,
+              style: TextStyle(
+                fontWeight: .w900,
+                fontSize: 20
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  void onPressedCalculateButton(){
+    double? input1 = double.tryParse(input1Controller.text.trim());
+    double? input2 = double.tryParse(input2Controller.text.trim());
+    if(input1==null || input2==null){
+      showDialog(
+        context: context, 
+        barrierDismissible: false,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('경고'),
+            content: Text('모든 칸에 숫자만을 입력하세요.'),
+            shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(5)
+                  ),
+            actionsAlignment: .center,
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(), 
+                child: Text('네 알겠습니다')
+              )
+            ],
+          );
+        },
+      );
+      resultString = '모든 칸에 숫자만을 입력하세요.';
+    }else if(input2 == 0){
+      showDialog(
+        context: context, 
+        barrierDismissible: false,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('경고'),
+            content: Text('0으로 나눌 수 없습니다.'),
+            shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(5)
+                  ),
+            actionsAlignment: .center,
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(), 
+                child: Text('네 알겠습니다')
+              )
+            ],
+          );
+        },
+      );
+      resultString = '0으로 나눌 수 없습니다.';
+    }else{
+      var inputNumber1 = input1%1==0?input1.toInt():input1;
+      var inputNumber2 = input2%1==0?input2.toInt():input2;
+      var resultNumber = input1/input2%1==0?(input1/input2).toInt():input1/input2;
+      resultString = '$inputNumber1 ÷ $inputNumber2 = $resultNumber';
+    }
+    setState(() {});
   }
 }
